e2943d0f2888e9f6d92df25b7f8d4f8c2cb978a1
Cano <contact.sasasaky@gmail.com>
2022-07-17 14:58:35 +0900

    minimun

--- main.go
+++ main.go
@@ -4,81 +4,113 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"runtime"
 	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
+func getConfigPath() string {
+	path := os.Getenv("HOME")
+
+	if path == "" && runtime.GOOS == "windows" {
+		path = os.Getenv("APPDATA")
+	} else {
+		path = filepath.Join(path, ".config")
+	}
+
+	path = filepath.Join(path, "gh-cgu")
+
+	return path
+}
+
 var flagSet bool
 
 var rootCmd = &cobra.Command{
-	Use: "cgu",
+	Use: "gh cgu",
 	Run: func(c *cobra.Command, args []string) {
-		if f, err := os.Stat(".git"); os.IsNotExist(err) || !f.IsDir() {
-			fmt.Println("Error: Not in a git directory")
-		} else {
 
-			// show current user
-			if len(args) == 0 {
-				userNameOut, _ := exec.Command("git", "config", "user.name").Output()
-				userEmailOut, _ := exec.Command("git", "config", "user.email").Output()
+		// show current user
+		// e.g. gh cgu
+		if len(args) == 0 {
+			userNameOut, _ := exec.Command("git", "config", "user.name").Output()
+			userEmailOut, _ := exec.Command("git", "config", "user.email").Output()
 
-				// erase \n ...
-				userName := strings.Replace(string(userNameOut), "\n", "", 1)
-				userEmail := strings.Replace(string(userEmailOut), "\n", "", 1)
+			// erase \n ...
+			userName := strings.Replace(string(userNameOut), "\n", "", 1)
+			userEmail := strings.Replace(string(userEmailOut), "\n", "", 1)
 
-				fmt.Printf("Current Git User: %s<%s>", userName, userEmail)
+			fmt.Printf("Current Git User: %s<%s>", userName, userEmail)
 
-				return
-			}
-
-			// set user
-			if flagSet && len(args) == 2 {
-				name := args[0]
-				email := args[1]
-
-				viper.Set(name+".name", name)
-				viper.Set(name+".email", email)
-				viper.WriteConfig()
-
-				fmt.Printf("Add Git User: %s<%s>", name, email)
-
-				return
-			}
-
-			// change user
-			if len(args) == 1 {
-				getName := args[0]
-
-				settingName := viper.GetString(getName + ".name")
-				settingEmail := viper.GetString(getName + ".email")
-
-				if settingName == "" || settingEmail == "" {
-					fmt.Println("Error: No such user")
-					return
-				}
-
-				exec.Command("git", "config", "user.name", settingName).Run()
-				exec.Command("git", "config", "user.email", settingEmail).Run()
-
-				fmt.Printf("Change Git User: %s<%s>", settingName, settingEmail)
-
-				return
-			}
-
-			c.Help()
+			return
 		}
 
+		// set user
+		// e.g. gh cgu --set username user@example.com
+		if flagSet && len(args) == 2 || len(args) == 3 {
+			name := args[0]
+			gitUserName := args[0]
+			gitUserEmail := args[1]
+
+			if len(args) == 3 {
+				gitUserName = args[1]
+				gitUserEmail = args[2]
+			}
+
+			viper.Set(name+".name", gitUserName)
+			viper.Set(name+".email", gitUserEmail)
+			viper.WriteConfig()
+
+			fmt.Printf("Add Git User: %s<%s>", gitUserName, gitUserEmail)
+
+			return
+		}
+
+		// change user
+		// e.g. gh cgu username
+		if len(args) == 1 {
+			if f, err := os.Stat(".git"); os.IsNotExist(err) || !f.IsDir() {
+				fmt.Println("Error: Not in a git directory")
+			}
+
+			getName := args[0]
+
+			settingName := viper.GetString(getName + ".name")
+			settingEmail := viper.GetString(getName + ".email")
+
+			if settingName == "" || settingEmail == "" {
+				fmt.Println("Error: No such user")
+				return
+			}
+
+			exec.Command("git", "config", "user.name", settingName).Run()
+			exec.Command("git", "config", "user.email", settingEmail).Run()
+
+			fmt.Printf("Change Git User: %s<%s>", settingName, settingEmail)
+
+			return
+		}
+
+		c.Help()
 	},
 }
 
 func main() {
 	cobra.OnInitialize(func() {
-		viper.SetConfigFile("config.toml")
-		viper.AddConfigPath(".")
+		configName := "config"
+		configType := "yaml"
+		configPath := getConfigPath()
 
-		viper.ReadInConfig()
+		viper.SetConfigName(configName)
+		viper.SetConfigType(configType)
+		viper.AddConfigPath(configPath)
+
+		if err := viper.ReadInConfig(); err != nil {
+			os.MkdirAll(configPath, 0700)
+			viper.WriteConfigAs(filepath.Join(configPath, fmt.Sprintf("%s.%s", configName, configType)))
+		}
 	})
 
 	rootCmd.PersistentFlags().BoolVarP(&flagSet, "set", "s", false, "set user")
